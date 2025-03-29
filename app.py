b5df072b06182bf26c001c73cb184e1ea4228a3d
Sreedhar Bhanavasi <Nasa>
2025-03-29 17:34:30 +0530

    My First Commit

--- app.py
+++ app.py
@@ -321,4 +321,5 @@ def catalog():
                          current_currency=currency)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
